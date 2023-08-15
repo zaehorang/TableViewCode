49b4d5b16bfe29e2ae0bfab0ec4a302382029fc3
zaehorang <zaehorang2@naver.com>
2023-08-16 00:03:41 +0900

    Initial Commit

---
+++ TableViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewCode
+//
+//  Created by zaehorang on 2023/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
